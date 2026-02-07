220bfe15fa5ebaeeac8b0318833174a5c8817c8a
dan <midonidaniel310712@gmail.com>
2026-02-07 14:14:02 +0200

    Build 1: -First version of the convertor

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "I am Dan" << "\n";
-    int a;
-    int b = a;
-    a = 9;
+    //Build 1
+    int n;
+    std::cout << "Simple ASCII Convertor" << std::endl;
+    std::cout << "How many times you want to convert?: ";
+    std::cin >> n;
+
+    char c;
+    for (int i = 0; i < n; i++) {
+        std::cout << "Enter character: ";
+        std::cin >> c;
+        std::cout << "Its ASCII value is: " << int(c) << std::endl;
+
+
+    }
     return 0;
 }
