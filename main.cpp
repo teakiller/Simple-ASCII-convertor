520a50ee127e8067692594f9d58ac98629af107e
dan <midonidaniel310712@gmail.com>
2026-02-08 17:02:12 +0200

    Build 1: -Converted the for loop into a do while loop

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 
 int main() {
-    //Build 1
+    //Build 2
     int n;
+    std::string exitcmd;
     std::cout << "Simple ASCII Convertor" << std::endl;
     std::cout << "How many times you want to convert?: ";
     std::cin >> n;
 
     char c;
-    for (int i = 0; i < n; i++) {
+    do{
         std::cout << "Enter character: ";
         std::cin >> c;
         std::cout << "Its ASCII value is: " << int(c) << std::endl;
 
+        if (c == )
 
-    }
+
+    }while (1 == 1);
     return 0;
 }
